05b7789475010520b7851eaf13c2e17f99472fdf
joshuacappelli <joshua@cappelliconnect.com>
2025-01-30 14:17:55 +0100

    deleted unecessary import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
-import { createSecretKey } from "crypto";
 
 export async function middleware(request: NextRequest) {
   // Basic debug logs
